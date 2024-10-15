e7008368f7a9d2fe007ac23ea10aaad4330060a2
Jeonghwan Kim <51739985+hwan20202@users.noreply.github.com>
2024-10-15 22:15:28 +0900

    Feat:조리순서 UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,16 @@ import SearchBar from "../components/SearchBar.jsx";
 import TitleWithItems from "../components/TitleWithItems.jsx";
 import MenuSearchForm from "../services/MenuSearchForm.jsx";
 import IngredientList from "../services/IngredientList.jsx";
+import CookingStepList from "../services/CookingStepList.jsx";
 
 const Home = () => {
     return (
-        <div className="Home-page w-screen">
+        <div className="Home-page w-full">
             <h1>Home</h1>
             <MenuSearchForm />
-            <IngredientList />
+            {/*<TitleWithItems title='Empty' items={[()=><></>]} className='mb-3'></TitleWithItems>*/}
+            <IngredientList className='mb-3' />
+            <CookingStepList />
         </div>
     );
 };
